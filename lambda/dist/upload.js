ec2ab50ad374768061c7dedc773a3e87a79edd1d
Mojo Talantikite <mojo.talantikite@gmail.com>
2020-11-02 15:42:53 -0500

    Update lambda filename

--- lambda/dist/upload.js
+++ lambda/dist/upload.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getPresignedUrl = void 0;
 const s3_1 = __importDefault(require("aws-sdk/clients/s3"));
+const uuid_1 = require("uuid");
 async function getPresignedUrl(event) {
     let url = await getUploadUrl();
     let res = {
@@ -21,9 +22,11 @@ async function getPresignedUrl(event) {
 exports.getPresignedUrl = getPresignedUrl;
 async function getUploadUrl() {
     let s3 = new s3_1.default();
+    let filename = uuid_1.v4();
     let params = {
-        Bucket: process.env.S3_BUCKET,
-        Key: 'test.jpg',
+        Bucket: process.env.UPLOAD_BUCKET,
+        Key: filename,
+        ContentType: 'image/jpeg',
         Expires: 60,
     };
     let url = s3.getSignedUrl('putObject', params);
--- lambda/src/upload.ts
+++ lambda/src/upload.ts
@@ -1,5 +1,6 @@
 import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
 import S3 from 'aws-sdk/clients/s3';
+import { v4 as uuidv4 } from 'uuid';
 
 export async function getPresignedUrl(event: APIGatewayEvent): Promise<APIGatewayProxyResult> {
     let url = await getUploadUrl();
@@ -17,9 +18,12 @@ export async function getPresignedUrl(event: APIGatewayEvent): Promise<APIGatewa
 
 async function getUploadUrl(): Promise<string> {
     let s3 = new S3();
+    let filename = uuidv4()
+
     let params = {
         Bucket: process.env.UPLOAD_BUCKET,
-        Key: 'test.jpg',
+        Key: filename,
+        ContentType: 'image/jpeg',
         Expires: 60, // 60 second expiry on URL
     }
 

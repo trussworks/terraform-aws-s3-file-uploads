ab29c4fedd8ee9b62aa0ef10e662081a3f1d8700
jsclarridge <2491291+jsclarridge@users.noreply.github.com>
2021-01-20 11:59:12 -0800

    Allow optional use of existing log bucket

--- s3.tf
+++ s3.tf
@@ -5,7 +5,7 @@ module "file_uploads_s3_bucket" {
   source         = "trussworks/s3-private-bucket/aws"
   version        = "~>3.2.0"
   bucket         = var.file_uploads_bucket
-  logging_bucket = module.file_uploads_s3_logging_bucket.aws_logs_bucket
+  logging_bucket = var.create_logging_bucket ? module.file_uploads_s3_logging_bucket.aws_logs_bucket : var.logging_bucket
 
   cors_rules = var.cors_rules
 
@@ -18,10 +18,11 @@ module "file_uploads_s3_bucket" {
 
 # we use a separate access logging bucket for every environment
 module "file_uploads_s3_logging_bucket" {
+  count = var.create_logging_bucket ? 1 : 0
   source  = "trussworks/logs/aws"
   version = "~> 10.0.0"
 
-  s3_bucket_name          = local.file_uploads_s3_bucket_logs
+  s3_bucket_name          = var.logging_bucket
   s3_log_bucket_retention = var.s3_logs_retention_days
 
   default_allow = false
